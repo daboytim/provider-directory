e26f8ea590d6ab424c0ea8d0c2c1409b80197e74
Derek Boytim <boytim.2@gmail.com>
2017-03-26 23:24:24 -0400

    Removed duplicate code in unit tests

--- app/provider-list/provider-list.component.spec.js
+++ app/provider-list/provider-list.component.spec.js
@@ -7,17 +7,18 @@ describe('providerList', function() {
   // Test the controller
   describe('controller', function() {
     var $httpBackend, ctrl;
+    var sampleProviders = [
+      {"last_name": "Harris", "first_name": "Mike", "email_address": "mharris@updox.com", "specialty": "Pediatrics", "practice_name": "Harris Pediatrics"},
+      {"last_name": "Wijoyo", "first_name": "Bimo", "email_address": "bwijoyo@updox.com", "specialty": "Podiatry", "practice_name": "Wijoyo Podiatry"},
+      {"last_name": "Rose", "first_name": "Nate", "email_address": "nrose@updox.com", "specialty": "Surgery", "practice_name": "Rose Cutters"},
+      {"last_name": "Carlson", "first_name": "Mike", "email_address": "mcarlson@updox.com", "specialty": "Orthopedics", "practice_name": "Carlson Orthopedics"},
+      {"last_name": "Witting", "first_name": "Mike", "email_address": "mwitting@updox.com", "specialty": "Pediatrics", "practice_name": "Witting’s Well Kids Pediatrics"},
+      {"last_name": "Juday", "first_name": "Tobin", "email_address": "tjuday@updox.com", "specialty": "General Medicine", "practice_name": "Juday Family Practice"}
+    ];
 
     beforeEach(inject(function($componentController, _$httpBackend_) {
       $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('providers/existingProviders.json').respond([
-        {"last_name": "Harris", "first_name": "Mike", "email_address": "mharris@updox.com", "specialty": "Pediatrics", "practice_name": "Harris Pediatrics"},
-          {"last_name": "Wijoyo", "first_name": "Bimo", "email_address": "bwijoyo@updox.com", "specialty": "Podiatry", "practice_name": "Wijoyo Podiatry"},
-          {"last_name": "Rose", "first_name": "Nate", "email_address": "nrose@updox.com", "specialty": "Surgery", "practice_name": "Rose Cutters"},
-          {"last_name": "Carlson", "first_name": "Mike", "email_address": "mcarlson@updox.com", "specialty": "Orthopedics", "practice_name": "Carlson Orthopedics"},
-          {"last_name": "Witting", "first_name": "Mike", "email_address": "mwitting@updox.com", "specialty": "Pediatrics", "practice_name": "Witting’s Well Kids Pediatrics"},
-          {"last_name": "Juday", "first_name": "Tobin", "email_address": "tjuday@updox.com", "specialty": "General Medicine", "practice_name": "Juday Family Practice"}
-      ]);
+      $httpBackend.expectGET('providers/existingProviders.json').respond(sampleProviders);
       ctrl = $componentController('providerList');
     }));
 
@@ -38,48 +39,31 @@ describe('providerList', function() {
 
     it('should add a provider to the list', function() {
       expect(ctrl.providers).toEqual([]);
-      var provider = {
-        "last_name": "Boytim",
-        "first_name": "Derek",
-        "email_address": "boytim.2@gmail.com",
-        "specialty": "Orthopedics",
-        "practice_name": "Derek's Joints"
-      };
-      ctrl.addProvider(provider);
+      ctrl.addProvider(sampleProviders[0]);
       expect(ctrl.providers).toEqual([
-        provider
+        sampleProviders[0]
       ]);
     });
 
     it('should remove a provider from the model', function() {
-      var provider = {
-        "last_name": "Boytim",
-        "first_name": "Derek",
-        "email_address": "boytim.2@gmail.com",
-        "specialty": "Orthopedics",
-        "practice_name": "Derek's Joints",
-        "checked": true
-      };
       ctrl.providers = [
-        provider
+        sampleProviders[0]
       ];
+      ctrl.providers[0].checked = true;
 
       ctrl.removeCheckedProviders();
       expect(ctrl.providers).toEqual([]);
     });
 
     it('should remove multiple providers from the model', function() {
-      var provider = {
-        "last_name": "Boytim",
-        "first_name": "Derek",
-        "email_address": "boytim.2@gmail.com",
-        "specialty": "Orthopedics",
-        "practice_name": "Derek's Joints",
-        "checked": true
-      };
       ctrl.providers = [
-        provider,provider,provider
+        sampleProviders[0],
+        sampleProviders[1],
+        sampleProviders[2]
       ];
+      ctrl.providers[0].checked = true;
+      ctrl.providers[1].checked = true;
+      ctrl.providers[2].checked = true;
 
       ctrl.removeCheckedProviders();
       expect(ctrl.providers).toEqual([]);
